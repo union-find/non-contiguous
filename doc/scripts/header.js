f9d24bbdab18632b596cdfee53e36d3107ff8e39
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2022-09-10 21:57:58 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/d09fbf6436be892b61bbe02454e1d7d7229c50d8/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/union-find/non-contiguous';
+	testlink.href = 'https://app.codecov.io/gh/union-find/non-contiguous';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
